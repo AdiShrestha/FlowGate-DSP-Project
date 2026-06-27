f73dc1eca9e7e7b413f12d491342049d0936111c
Aditya Shrestha <yours.only.adi@gmail.com>
2026-06-27 07:22:52 +0545

    chore: Add Apache 2.0 license header to DeLong implementation for IEEE compliance

--- load-adaptive-iir/src/delong.py
+++ load-adaptive-iir/src/delong.py
@@ -2,8 +2,25 @@ import pandas as pd
 import numpy as np
 import scipy.stats
 
-# AUC comparison adapted from
-# https://github.com/Netflix/vmaf/
+# ==============================================================================
+# The following DeLong ROC AUC implementation is adapted from the Netflix VMAF
+# project (https://github.com/Netflix/vmaf/).
+# 
+# Copyright 2016-2020 Netflix, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
 def compute_midrank(x):
     """Computes midranks.
     Args:
